4f46c6877cf769539f2ce9144d5e19d0f8e4e495
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-16 23:31:48 +0545

    updated: use of useState hook and updated: interview type question in comment

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -13,21 +13,45 @@ function App() {
   // let counter = 15
 
 
-  //youtube chai our code:
-  // const addvalue = ()=>{
-  //   console.log("value added", counter)
-  //   setcounter(counter +1)
-  // }
 
-  // const removecalue = ()=>{
+  
+
+  //youtube chai our code:
+//   const addValue = ()=>{
+//     console.log("value added", counter)
+//     setcounter(counter +1)
+//     setcounter(counter +1)
+//     setcounter(counter +1)
+//     setcounter(counter +1)
+//  // the interview question is that will the initial value ie 15 be directly incremented to 19 or what? => no the initial value will not be incremented directly to 19 because the     setcounter(counter +1) is calling the same current state so it will only increment by 1 no matter what.
+
+//   }
+//  const addValue = ()=>{
+//     console.log("value added", counter)
+//     setcounter(prevCounter  => prevCounter +1)
+//     setcounter(prevCounter  => prevCounter +1)
+//     setcounter(prevCounter  => prevCounter +1)
+//     setcounter(prevCounter  => prevCounter +1)
+//   // now talking about this , the setcounter built in comes with a callback function so it fetches the current value of counter and named it as whatever(prevCounter) so it takes that and adds with + 1, and again another call another +1...., and imp thing>> the useState takes the batches of value first and sends it for display 
+// // here it will surely incremented by +4 directly to make its intial value to be incremented to 19
+
+//   }
+
+
+  // const removeValue = ()=>{
   //   console.log("value removed", counter)
   //   setcounter(counter -1)
   // }
 
 
+
+
+
+
+
 // youtube:assignment addvalue should be no greater than 20 and removevalue should be no smaller than 0
 
-const addvalue = ()=>{
+const addValue = ()=>{
   if(counter >= 20){
     console.log("the counter value should not be greater than 20")
   }
@@ -51,7 +75,7 @@ const removeValue = ()=>{
       <h1>Chai our code</h1>
       <h2>counter value : {counter}</h2>
 
-      <button onClick={addvalue}>Add value</button>
+      <button onClick={addValue}>Add value</button>
       <br />
       <button onClick={removeValue}>remove value</button>
 
@@ -60,3 +84,5 @@ const removeValue = ()=>{
 }
 
 export default App
+
+
