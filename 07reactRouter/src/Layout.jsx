d57b4cbe3c997170151c6d2a894c277de3b6688e
iamkapilll <kapildevchaudhary067@gmail.com>
2025-07-25 23:28:32 +0545

    react router

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom' // we need to inport this for outlet
 
 function Layout() {
   return (
