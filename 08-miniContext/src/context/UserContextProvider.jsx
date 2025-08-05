bdd53aa46ef75f7226a6fe0097960522c54ec11f
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-05 19:17:03 +0545

    Usercontext.provider initial setupp

--- 08-miniContext/src/context/UserContextProvider.jsx
+++ 08-miniContext/src/context/UserContextProvider.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = ({children}) =>{
+const UserContextProvider = ({children}) =>{ // here it will take value from where it is sent and later it is wrapped inside the UserContext.Provider
     const [user, setUser] = useState(null)
 
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser}}> 
         {children}
         </UserContext.Provider>
     )
