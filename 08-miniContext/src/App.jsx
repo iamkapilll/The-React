44223f3c85bac0f8fe66927e3f4c5ae522ce0a6a
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-05 19:21:41 +0545

    initial setup for UserContextProvider in App.jsx

--- 08-miniContext/src/App.jsx
+++ 08-miniContext/src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
-    <>
+    <UserContextProvider>
      <h1>React with chai</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
