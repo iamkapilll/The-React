8002375b39e13e013317aa28cec8adb6e441efdd
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-08 03:46:20 +0545

    Updated Appjsx

--- 08-miniContext/src/App.jsx
+++ 08-miniContext/src/App.jsx
@@ -1,12 +1,15 @@
-
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
-
+//here we will get the data from usercontextprovideer and wrap them
 function App() {
 
   return (
     <UserContextProvider>
      <h1>React with chai</h1>
+     <Login />
+     <Profile />
     </UserContextProvider>
   )
 }
