d731ac1d838c6ecbc61ea668b5689e644c55cb5a
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-07 07:38:41 +0545

    updated usecurrency info

--- 06currencyconverter/src/hooks/useCurrencyInfo.js
+++ 06currencyconverter/src/hooks/useCurrencyInfo.js
@@ -1,17 +1,17 @@
-import { useState, useEffect } from "react";
+import {useEffect, useState} from "react"
 
-function useCurrencyInfo(currency) {
-  const [data, setData] = useState({}); // State to store fetched currency data
 
-  useEffect(() => {
-    // Fetch data from API when 'currency' changes
-    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-      .then((res) => res.json())
-      .then((res) => setData(res[currency]))
-      .catch((err) => console.error("Failed to fetch currency data:", err));
-  }, [currency]);
-  console.log(data)
-  return data;
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
 }
 
 export default useCurrencyInfo;
+
