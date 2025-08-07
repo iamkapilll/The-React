f7f197f5c459eded02e963bf27377be8937d58e8
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-07 18:17:16 +0545

    edited UserContesxt.jsx

--- 08-miniContext/src/context/UserContext.js
+++ 08-miniContext/src/context/UserContext.js
@@ -3,3 +3,6 @@ import React from "react";
 const UserContext = React.createContext()
 
 export default UserContext
+ 
+
+//this context will be used to share user data across components thats why it is called as global context
