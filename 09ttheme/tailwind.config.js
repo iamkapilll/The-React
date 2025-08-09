4b3e03c85625c00ec314d56ea033b9cb034cce28
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-09 23:45:22 +0545

    tailwind config

--- 09ttheme/tailwind.config.js
+++ 09ttheme/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
