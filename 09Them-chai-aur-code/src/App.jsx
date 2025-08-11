719863128b24b55c4da63fea4758d7af5f02d6ab
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-11 17:40:20 +0545

    ThemeBtn and Card component will be added later

--- 09Them-chai-aur-code/src/App.jsx
+++ 09Them-chai-aur-code/src/App.jsx
@@ -8,11 +8,11 @@ function App() {
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* ThemeBtn */}
+                        {/* ThemeBtn component */}
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       {/*  */}
+                       {/* Card component */}
                     </div>
                 </div>
             </div>
