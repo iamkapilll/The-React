9625732abee56a28526856aa25ca0e829ac73a83
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-11 19:08:24 +0545

    ThemeContext, ThemeProvider and useTheme is created in a single file for better approach

---
+++ 09Them-chai-aur-code/src/contexts/theme.js
@@ -0,0 +1,13 @@
+import {createContext, usecontext} from "react"
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme: () => {},
+    lightTheme: () => {},
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return usecontext(ThemeContext)
+}
