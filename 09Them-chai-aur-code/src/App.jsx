54cba9b2b9c976dacbe451b94e7d00a011bce72b
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-11 19:00:45 +0545

    ThemeProvider (wrapper) is done for app.jsx

--- 09Them-chai-aur-code/src/App.jsx
+++ 09Them-chai-aur-code/src/App.jsx
@@ -1,22 +1,23 @@
 
+import ThemeProvider from '../../09The-themeswitcher/src/ThemeProvider'
 import './App.css'
 
 function App() {
 
   return (
-   
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* ThemeBtn component */}
-                    </div>
+    <ThemeProvider>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* ThemeBtn component */}
+        </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       {/* Card component */}
-                    </div>
-                </div>
-            </div>
-
+        <div className="w-full max-w-sm mx-auto">
+          {/* Card component */}
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
   )
 }
 
