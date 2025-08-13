e0f573a324f1381a58422d9b9f9f0e2960f8c4b8
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-13 22:25:51 +0545

    UI added: chai-code-github

--- 10todo-ContextLocalstorage/src/App.jsx
+++ 10todo-ContextLocalstorage/src/App.jsx
@@ -5,9 +5,17 @@ function App() {
 
   return (
     <>
-     <div className='bg-gray-800 text-white p-4 rounded-lg'>hello world from kapil dev chaudhary
-      hi hi hello world
-      </div>
+     <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
    </>
   )
 }
