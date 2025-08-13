4689892af178fc87f23a6d480ab53845e7c134bd
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-13 22:32:41 +0545

    wrapping App.jsx with TodoProvider

--- 10todo-ContextLocalstorage/src/App.jsx
+++ 10todo-ContextLocalstorage/src/App.jsx
@@ -1,13 +1,14 @@
 
 import { useState } from 'react'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
 
   const [todos. setTodos] = useState([])
 
   return (
-    <>
+    <TodoProvider>
      <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -19,7 +20,7 @@ function App() {
                     </div>
                 </div>
             </div>
-   </>
+   </TodoProvider>
   )
 }
 
