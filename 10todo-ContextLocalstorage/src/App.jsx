f9d6ec87a350eb9f26d5f5f00593011bb5295c99
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-16 22:14:27 +0545

    injected components

--- 10todo-ContextLocalstorage/src/App.jsx
+++ 10todo-ContextLocalstorage/src/App.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { TodoProvider } from './contexts'
+import TodoItem from './components/TodoItem'
+import TodoForm from './components/TodoForm'
 
 function App() {
 
@@ -47,9 +49,17 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) =>(
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
                     </div>
                 </div>
    </TodoProvider>
