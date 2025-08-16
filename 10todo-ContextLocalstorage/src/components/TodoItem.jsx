5c2d1577e38e5cba5b98d83fda539a34dd7af738
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-16 21:43:47 +0545

    document formated and alligned

--- 10todo-ContextLocalstorage/src/components/TodoItem.jsx
+++ 10todo-ContextLocalstorage/src/components/TodoItem.jsx
@@ -3,14 +3,15 @@ import { useTodo } from '../contexts';
 
 function TodoItem({ todo }) {
 
-  const {updateTodo, deleteTodo, toggleCompleted} = useTodo //extracting whatever you want from TodoContext and make functionality here
-    
+
+
+    const { updateTodo, deleteTodo, toggleCompleted } = useTodo //extracting whatever you want from TodoContext and make functionality here
+
 
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
-                todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
-            }`}
+            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
+                }`}
         >
             <input
                 type="checkbox"
@@ -20,9 +21,8 @@ function TodoItem({ todo }) {
             />
             <input
                 type="text"
-                className={`border outline-none w-full bg-transparent rounded-lg ${
-                    isTodoEditable ? "border-black/10 px-2" : "border-transparent"
-                } ${todo.completed ? "line-through" : ""}`}
+                className={`border outline-none w-full bg-transparent rounded-lg ${isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                    } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
                 readOnly={!isTodoEditable}
