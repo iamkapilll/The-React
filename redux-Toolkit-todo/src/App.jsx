d88c26a53babed5cc69aca56b8fa6d4c807740dc
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-21 01:05:30 +0545

    components injected in App.jsx

--- redux-Toolkit-todo/src/App.jsx
+++ redux-Toolkit-todo/src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import AddTodo from './components/addTodo'
+import Todos from './components/Todos'
 
 function App() {
 
   return (
     <>
      <h1>Learning Redux Toolkit</h1>
+
+     <AddTodo />
+     <Todos />
     </>
   )
 }
