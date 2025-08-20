eac5991f5c27ccb0bf44447361cc3c543af0e1f5
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-21 00:50:55 +0545

    in progress

--- redux-Toolkit-todo/src/components/Todos.jsx
+++ redux-Toolkit-todo/src/components/Todos.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+
+
 
 const Todos = () => {
+
+    const todos = useSelector(state => state.todos)  // useSelector for accessing or selectiong initial state from todoSlice.js
+    const dispatch = useDispatch()
+
   return (
     <div>Todos</div>
   )
