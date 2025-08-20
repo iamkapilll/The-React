02cebb4cf9f39e6d03040e5429bd7280f28063b1
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-21 00:46:23 +0545

    updation

--- redux-Toolkit-todo/src/components/addTodo.jsx
+++ redux-Toolkit-todo/src/components/addTodo.jsx
@@ -5,7 +5,6 @@ import { addTodo } from '../features/todo/todoSlice'
 function AddTodo() {
 
     const [input, setInput] = useState("")
-
     const dispatch = useDispatch()
 
     const addTodoHandler = (e) =>{
