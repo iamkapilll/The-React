a3e9ece76c12b45dfb7cf2ebdf26eba00852e543
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-22 09:33:39 +0545

    updation

--- redux-Toolkit-todo/src/app/store.js
+++ redux-Toolkit-todo/src/app/store.js
@@ -1,6 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit'
-import todoReducer from '../features/todo/todoSlice';
+// Import configureStore from Redux Toolkit
+import { configureStore } from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice'
 
+// Create the store and register our todoSlice reducer
 export const store = configureStore({
-    reducer: todoReducer  //key value
+    reducer: todoReducer  // We directly set todoReducer (state shape will match reducer's initialState)
 })
+
+// configureStore creates the store (where your entire app state lives).
+
+// todoReducer is coming from todoSlice.js, which manages todos.
