a3098ce45a25676a8d72e851014ff57945f84965
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-23 06:16:49 +0545

    setEndpoints with constructor

--- 12Mega-Blog/src/appwrite/auth.js
+++ 12Mega-Blog/src/appwrite/auth.js
@@ -5,6 +5,13 @@ import {Client, Account, ID} from "appwrite";
 export class AuthService {
     client = new Client();
     account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setEndpoint(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
 }
 
 const authService = new AuthService();
