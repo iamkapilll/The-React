eb89b92b381b190620479bc2aa1616466e5e2af8
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-23 06:49:49 +0545

    throw error causing red underline error, ignore this

--- 12Mega-Blog/src/appwrite/auth.js
+++ 12Mega-Blog/src/appwrite/auth.js
@@ -1,19 +1,43 @@
+// src/appwrite/auth.js
 import conf from '../conf.js'
-import {Client, Account, ID} from "appwrite";
-
+import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
     client = new Client();
     account;
 
-    constructor(){
+    constructor() {
         this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setEndpoint(conf.appwriteProjectId);
+            .setEndpoint(conf.appwriteUrl)       // API endpoint (e.g., https://cloud.appwrite.io/v1)
+            .setProject(conf.appwriteProjectId); // Project ID
         this.account = new Account(this.client);
     }
+
+    // ✅ Create new account
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+            if(userAccount){
+                // return userAccount; call another function like account created then go for login
+            }else{
+                return userAccount
+            }
+            
+        } catch (error) {
+            throw error;
+            // so the red line error is due to the throw error; we can handle it in the calling function
+        }
+    }
+
 }
 
+// Create a single instance to use across app
 const authService = new AuthService();
 
-export default authService
+export default authService;
+
