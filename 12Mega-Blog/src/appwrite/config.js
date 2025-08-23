8612deb726ac4eac4870a75cfdc78d2f14708246
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-24 04:46:13 +0545

    config.js for services

---
+++ 12Mega-Blog/src/appwrite/config.js
@@ -0,0 +1,9 @@
+import conf from '../conf.js';
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{}
+
+
+
+const service = new Service();
+export default service;
