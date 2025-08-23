e400091822944a2725f73ff5e1dba40ea1d3f0bf
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-23 06:12:59 +0545

    building an authentication service with appwrite

---
+++ 12Mega-Blog/src/appwrite/auth.js
@@ -0,0 +1,12 @@
+import conf from '../conf.js'
+import {Client, Account, ID} from "appwrite";
+
+
+export class AuthService {
+    client = new Client();
+    account;
+}
+
+const authService = new AuthService();
+
+export default authService
