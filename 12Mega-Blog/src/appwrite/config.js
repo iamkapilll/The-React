6e0c88d13d593cf3e08e1495c203dbb64f03f177
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-24 05:07:16 +0545

    createPost() method complete

--- 12Mega-Blog/src/appwrite/config.js
+++ 12Mega-Blog/src/appwrite/config.js
@@ -13,7 +13,25 @@ export class Service{
             this.databases = new Databases(this.client);
             this.bucket = new Storage(this.client) //bucket = stroage // bucket is just a variable name for storage
     }
-    
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status, 
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: errro", error)
+        }
+    }
 }
 
 
