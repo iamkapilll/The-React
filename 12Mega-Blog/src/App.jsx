4ce9b4a4d06f03d3b83c67478ab3852e46500ece
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-26 05:29:34 +0545

    use of dispatch with userData for login and logout and state

--- 12Mega-Blog/src/App.jsx
+++ 12Mega-Blog/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {useDispatch} from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
@@ -9,7 +9,17 @@ function App() {
   const [loading, setLoading] = useState(true) // at first it should be true for If user loggin or not
   const dispatch = useDispatch()
 
-
+  useEffect(() =>{
+    authService.getCurrentUser()
+      .then((userData) =>{
+        if(userData){
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
   
 
   return (
