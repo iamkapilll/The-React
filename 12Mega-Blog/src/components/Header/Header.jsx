dfc73e7fb7b37985bbc286912d060bdcaaf79e8f
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-27 04:30:57 +0545

    Navbar buttons with condition of authStatus

--- 12Mega-Blog/src/components/Header/Header.jsx
+++ 12Mega-Blog/src/components/Header/Header.jsx
@@ -20,12 +20,12 @@ function Header() {
     {
       name: "Login",
       slug: "/login",
-      active: !authStatus,
+      active: !authStatus,  // if authstatus true then show login 
   },
   {
       name: "Signup",
       slug: "/signup",
-      active: !authStatus,
+      active: !authStatus, // if authstatus true then show signup
   },
   {
       name: "All Posts",
@@ -38,10 +38,37 @@ function Header() {
       active: authStatus,
   },
   ]
+ return (
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px'   />
 
-  return (
-    <div>Header</div>
+              </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+        </Container>
+    </header>
   )
 }
 
-export default Header  
+export default Header
