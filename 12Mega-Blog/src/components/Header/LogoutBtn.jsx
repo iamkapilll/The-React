91c3afc055fa9192950183eb2b529bb4e15fc7c9
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-27 03:51:03 +0545

    LogoutHandler() wiht dispatch for logout functionality

--- 12Mega-Blog/src/components/Header/LogoutBtn.jsx
+++ 12Mega-Blog/src/components/Header/LogoutBtn.jsx
@@ -6,12 +6,12 @@ import { logout } from '../../store/authSlice'
 
 function LogoutBtn() {
 
-    // const dispatch = useDispatch()
-    // const logoutHandler = () => {
-    //     authService.logout().then(() =>{
-    //         dispatch(logout())
-    //     })
-    // }
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authService.logout().then(() =>{
+            dispatch(logout())
+        })
+    }
 
   return (
     <div>LogoutBtn</div>
