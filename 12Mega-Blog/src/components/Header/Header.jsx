b3178d56a01d3a2a61817f76fabf752cba88fc93
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-27 04:07:49 +0545

    useSelector for status

--- 12Mega-Blog/src/components/Header/Header.jsx
+++ 12Mega-Blog/src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Container, Logo, LogoutBtn} from '../index'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+
+  const authStatus = useSelector((state) => state.auth.status)  //see authSLice
+
   return (
     <div>Header</div>
   )
