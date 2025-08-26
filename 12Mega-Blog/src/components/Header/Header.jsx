7eec8c549e2d6ba86ca967758fb80d0f73af81d3
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-27 04:18:33 +0545

    Navigation items using useNavigate

--- 12Mega-Blog/src/components/Header/Header.jsx
+++ 12Mega-Blog/src/components/Header/Header.jsx
@@ -9,6 +9,36 @@ function Header() {
   //authStatus to see status true or false
   const authStatus = useSelector((state) => state.auth.status)  //see authSLice
 
+    const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  },
+  ]
+
   return (
     <div>Header</div>
   )
