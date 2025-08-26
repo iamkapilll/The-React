9821f13e46799794b058032cb75a520b5c124499
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-27 05:01:10 +0545

    A common button

---
+++ 12Mega-Blog/src/components/Button.jsx
@@ -0,0 +1,12 @@
+//just a common button used for different places
+import React from 'react'
+
+function Button() {
+  return (
+    <div>Button</div>
+  )
+}
+
+export default Button
+
+//{children} is like a parameter or some content or text that is passed to the button
