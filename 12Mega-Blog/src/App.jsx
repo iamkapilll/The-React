9e17c5c3f2eb42e0c5cb0f52fd2f595537985de5
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-26 06:20:04 +0545

    conditional rendering for app.jsx

--- 12Mega-Blog/src/App.jsx
+++ 12Mega-Blog/src/App.jsx
@@ -20,13 +20,19 @@ function App() {
       })
       .finally(() => setLoading(false))
   }, [])
-  
 
-  return (
-    <>
-    <h1>Blog App with Appwrite</h1>
-    </>
-  )
+
+ return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        {/* TODO:  <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App  
