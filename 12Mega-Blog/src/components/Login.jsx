06686586a6e3ba90135d9cd60d85cc702a145d14
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-30 21:12:33 +0545

    Login component

--- 12Mega-Blog/src/components/Login.jsx
+++ 12Mega-Blog/src/components/Login.jsx
@@ -21,6 +21,7 @@ function Login() {
             if(session){
                 const userData = await authService.getCurrentUser()
                 if(userData) dispatch(authLogin(userData))  //dispatch with reducer to send userData
+                 navigate("/")
             }
         } catch (error) {
             setError(error.message)
