fffc9eb5d51557dfeeb014313f28e6852b35b5a9
iamkapilll <kapildevchaudhary067@gmail.com>
2025-08-30 17:19:04 +0545

    PostCard for file Previews

--- 12Mega-Blog/src/components/PostCard.jsx
+++ 12Mega-Blog/src/components/PostCard.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+//step 9
 
-function PostCard() {
+import React from 'react'
+import appwriteService from "../appwrite/config"  //we will be using filepreview from that file
+import { Link } from 'react-router-dom'
+
+function PostCard({$id, title, featuredImage}) { // $id is the default behaviour of appwrite so we need to use $ sign
+
   return (
-    <div>PostCard</div>
+    <Link to={`/post/${$id}`}> 
+         <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full justify-center mb-4'>
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+                className='rounded-xl' />
+
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
+        </div>
+    </Link>
   )
 }
 
 export default PostCard
+
+ // <img src={appwriteService.getFilePreview(featuredImage)} alt={title} // appwriteService bata getFilePreview use garera  
